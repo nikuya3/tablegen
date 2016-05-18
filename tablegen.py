69973a733c72ba0b7e7fba45add160d591d41915
nikuya3 <nikuya@dummy.mail>
2016-05-18 15:56:06 +0200

    Created type selection

--- tablegen.py
+++ tablegen.py
@@ -3,11 +3,11 @@ import csv
 import codecs
 import sys
 
-stmt = 'CREATE TABLE';
+stmt = 'CREATE TABLE'
 primary_key = ''
 reader = None
 
-parser = argparse.ArgumentParser(description='Generates an SQL CREATE TABLE statement from a CSV file')
+"""parser = argparse.ArgumentParser(description='Generates an SQL CREATE TABLE statement from a CSV file')
 parser.add_argument('integers', metavar='N', type=int, nargs='+',
                     help='an integer for the accumulator')
 parser.add_argument('--sum', dest='accumulate', action='store_const',
@@ -17,10 +17,16 @@ args = parser.parse_args()
 
 # Check arguments
 for arg in sys.argv:
-    print(arg)
+    print(arg)"""
 
 
 def is_integer(s):
+    """Checks whether the given object is an integer.
+    Args:
+        s: The object to be checked.
+    Returns:
+        A boolean value indicating whether the given object is an integer.
+    """
     try:
         int(s)
         return True
@@ -28,27 +34,86 @@ def is_integer(s):
         return False
 
 
-def is_decimal(s):
+def is_float(s):
+    """Checks whether the given object is a floating point value.
+    Args:
+        s: The object to be checked.
+    Returns:
+        A boolean value indicating whether the given object is a floating point value.
+    """
     try:
         float(s)
         return True
     except ValueError:
         return False
 
-def get_sql_type(colnum):
-    decimal = False
-    integer = False
+
+def is_boolean(s):
+    """Checks whether the given object is a boolean value.
+    Args:
+        s: The object to be checked.
+    Returns:
+        A boolean value indicating whether the given object is an integer.
+    """
+    s = s.lower()
+    return s == 'true' or s == 'false' or s == 'yes' or s == 'no'
+
+
+def is_binary(s):
+    """Checks whether the given object is an integer.
+    Args:
+        s: The object to be checked.
+    Returns:
+        A boolean value indicating whether the given object is an integer.
+    """
+    try:
+        int(s, 2)
+        return True
+    except ValueError:
+        return False
+
+
+def get_col_sql_type(colnum):
+    detected_binary = True
+    detected_boolean = True
+    detected_float = True
+    detected_integer = True
     rownum = 0
-    for row in reader:
+    longest_value = 0
+    for row in rows:
         if rownum == 0:
+            rownum += 1
             continue
         num = 0
-        for col in row:
+        for column in row:
             if num == colnum:
-                decimal = is_decimal(col)
+                if len(column) > 0:
+                    if detected_binary:
+                        print(column + ',' + 'binary')
+                        detected_binary = is_binary(column)
+                    if detected_boolean:
+                        print(column + ',' + 'boolean')
+                        detected_boolean = is_binary(column)
+                    if detected_float:
+                        print(column + ',' + 'float')
+                        detected_float = is_float(column)
+                    if detected_integer:
+                        print(column + ',' + 'integer')
+                        detected_integer = is_integer(column)
+                    if longest_value < len(column):
+                        longest_value = len(column)
             num += 1
         rownum += 1
-    return 'varchar(80)'
+    if detected_binary:
+        return 'binary'
+    if detected_boolean:
+        return 'boolean'
+    if detected_float and not detected_integer:
+        return 'float'
+    if detected_integer:
+        return 'integer'
+    return 'varchar(' + str(longest_value) + ')'
+
 
 # Read CSV file
 filename = 'test.csv'
@@ -56,12 +121,15 @@ with codecs.open(filename, 'r', 'utf-8') as csvfile:
     stmt += ' ' + csvfile.name[:len(filename) - 4] + ' ('
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     rownum = 0
+    rows = []
     for row in reader:
+        rows.append(row)
+    for row in rows:
         if rownum == 0:
             colnum = 0
             for col in row:
                 stmt += ' ' + col
-                stmt += ' ' + get_sql_type(col)
+                stmt += ' ' + get_col_sql_type(colnum)
                 if colnum == 0:
                     stmt += ' PRIMARY KEY'
                 stmt += ','
